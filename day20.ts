aae93364035930d39fcf8683d0fcaa9d4f594041
t3chcrazy <abhishekprashant09@gmail.com>
2023-12-20 23:18:48 +0530

    Day 20: Hack way, todo: optimize further

---
+++ day20.ts
@@ -0,0 +1,105 @@
+type Letters = {
+    A: [
+      '█▀█ ',
+      '█▀█ ',
+      '▀ ▀ ',
+    ],
+    B: [
+      '█▀▄ ',
+      '█▀▄ ',
+      '▀▀  '
+    ],
+    C: [
+      '█▀▀ ',
+      '█ ░░',
+      '▀▀▀ '
+    ],
+    E: [
+      '█▀▀ ',
+      '█▀▀ ',
+      '▀▀▀ '
+    ],
+    H: [
+      '█ █ ',
+      '█▀█ ',
+      '▀ ▀ '
+    ],
+    I: [
+      '█ ',
+      '█ ',
+      '▀ '
+    ],
+    M: [
+      '█▄░▄█ ',
+      '█ ▀ █ ',
+      '▀ ░░▀ '
+    ],
+    N: [
+      '█▄░█ ',
+      '█ ▀█ ',
+      '▀ ░▀ '
+    ],
+    P: [
+      '█▀█ ',
+      '█▀▀ ',
+      '▀ ░░'
+    ],
+    R: [
+      '█▀█ ',
+      '██▀ ',
+      '▀ ▀ '
+    ],
+    S: [
+      '█▀▀ ',
+      '▀▀█ ',
+      '▀▀▀ '
+    ],
+    T: [
+      '▀█▀ ',
+      '░█ ░',
+      '░▀ ░'
+    ],
+    Y: [
+      '█ █ ',
+      '▀█▀ ',
+      '░▀ ░'
+    ],
+    W: [
+      '█ ░░█ ',
+      '█▄▀▄█ ',
+      '▀ ░ ▀ '
+    ],
+    ' ': [
+      '░',
+      '░',
+      '░'
+    ],
+    ':': [
+      '#',
+      '░',
+      '#'
+    ],
+    '*': [
+      '░',
+      '#',
+      '░'
+    ],
+  };
+  
+  type ToAsciiLine<
+    T extends string,
+    Count extends any[] = [],
+    TempString extends string = "",
+    Result extends any[] = [],
+    Iterator extends string = T,
+  > = Iterator extends `${infer C extends keyof Letters}${infer R}`? (
+      ToAsciiLine<T, Count, `${TempString}${Letters[C][Count["length"]]}`, Result, R>
+  ): (
+    Count['length'] extends 3? Result: ToAsciiLine<T, [-1, ...Count], "", [...Result, TempString]>
+  )
+  
+  type ToAsciiArt<
+    T extends string,
+    Result extends string[] = [],
+    TempT = `${T}\n test`
+  > = TempT extends `${infer L}\n${infer R}`? ToAsciiArt<T, [...Result, ...ToAsciiLine<Uppercase<L>>], R>: Result
